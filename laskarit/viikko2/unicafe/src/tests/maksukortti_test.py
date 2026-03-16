7fda6421cc14db8386abf08f4e2dd8b4206d5795
eesalor <197505674+eesalor@users.noreply.github.com>
2026-03-16 20:09:54 +0200

    New tests to unicafe/src/tests/maksukortti_test.py

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(self.maksukortti.saldo, 2000)
+
+    def test_saldo_vahenee_oikein_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo, 500)
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    def test_rahan_ottaminen_toimii_1(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(500))
+
+    def test_rahan_ottaminen_toimii_2(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(1500))
