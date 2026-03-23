93a8d481ede0834517eb830513cc7e57e6de7615
eesalor <197505674+eesalor@users.noreply.github.com>
2026-03-23 13:11:50 +0200

    Create a class for tasks

---
+++ src/entities/task.py
@@ -0,0 +1,9 @@
+class Task:
+    def __init__(self, title):
+        self.title = title
+
+    def __str__(self):
+        return f"{self.title}"
+    
+    def __repr__(self):
+        return f"{self.title}"
