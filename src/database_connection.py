410eda4d951a239188598a47d2770f989826386f
eesalor <197505674+eesalor@users.noreply.github.com>
2026-03-26 15:17:49 +0200

    Create env variables / Add dependencies python-dotenv, pytest-dotenv, pytest

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,6 +1,7 @@
 import sqlite3
+from config import DATABASE
 
-connection = sqlite3.connect("database.db")
+connection = sqlite3.connect(DATABASE)
 connection.isolation_level = None
 connection.row_factory = sqlite3.Row
 
