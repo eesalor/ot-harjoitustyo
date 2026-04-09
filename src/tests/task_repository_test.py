b08e2cc3091579f56bfe7e1e5a495ab1ca771e62
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-09 14:51:54 +0300

    Unittest for task repository: delete task / Fix test get all tasks

--- src/tests/task_repository_test.py
+++ src/tests/task_repository_test.py
@@ -7,6 +7,7 @@ class TestTaskRepository(unittest.TestCase):
     def setUp(self):
         self.database = task_repository
         initialize_database()
+
         self.task1 = Task("Write some unit tests", "2026-04-04")
         self.task2 = Task("Refactor your code", "2026-05-05")
 
@@ -24,4 +25,15 @@ class TestTaskRepository(unittest.TestCase):
         self.database.create_task(self.task2)
         tasks = self.database.get_all_tasks()
 
-        self.assertEqual(tasks, ["Write some unit tests 2026-04-04", "Refactor your code 2026-05-05"])
+        self.assertEqual(tasks, {1: "Write some unit tests 2026-04-04", 2: "Refactor your code 2026-05-05"})
+
+    def test_delete_task(self):
+        self.database.create_task(self.task1)
+        self.database.create_task(self.task2)
+
+        self.database.delete_task(1)
+        self.database.delete_task(2)
+
+        tasks = self.database.get_all_tasks()
+
+        self.assertEqual(tasks, {})
