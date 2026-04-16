2af02c8bc379040fbde552aa984f02f712b15ea7
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-16 13:59:10 +0300

    Refactor task_view.py

--- src/ui/task_view.py
+++ src/ui/task_view.py
@@ -8,8 +8,8 @@ class TaskView:
         self._root = root
         self._frame = None
         self._update_task_view = update_task_view
-        self._task_title = None
-        self._task_date = None
+        self._task_title_entry = None
+        self._task_date_entry = None
         self._all_tasks = None
         self._listbox = None
         self._listbox_dictionary = None
@@ -27,28 +27,28 @@ class TaskView:
         self._frame = ttk.Frame(master=self._root)
 
         self._show_task_list()
-    
-    def _show_task_list(self):
+
+    def _create_task_form(self):
 
         create_label= ttk.Label(master=self._frame, text="Create new task")
         task_title_label = ttk.Label(master=self._frame, text="Task")
         task_date_label = ttk.Label(master=self._frame, text="Date")
 
-        self._task_title = ttk.Entry(master=self._frame)
-        self._task_date = ttk.Entry(master=self._frame)
+        self._task_title_entry = ttk.Entry(master=self._frame)
+        self._task_date_entry = ttk.Entry(master=self._frame)
         
-        button = ttk.Button(master=self._frame, text="Create", command=self._handle_create_button_click)
+        create_button = ttk.Button(master=self._frame, text="Create", command=self._handle_create_button_click)
 
         create_label.grid(padx=5, pady=5)
         task_title_label.grid(row=0, column=1,padx=5, pady=5)
         task_date_label.grid(row=0, column=2, padx=5, pady=5)
 
-        self._task_title.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
-        self._task_date.grid(row=1, column=2, sticky=(constants.E, constants.W), padx=5, pady=5)
-        
-        button.grid(row=1, column=3, sticky=(constants.E, constants.W))
-        self._all_tasks = self._get_all_tasks()
-        
+        self._task_title_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        self._task_date_entry.grid(row=1, column=2, sticky=(constants.E, constants.W), padx=5, pady=5)
+
+        create_button.grid(row=1, column=3, sticky=(constants.E, constants.W))
+
+    def _create_task_listbox(self):
         label = ttk.Label(master=self._frame, text="Task list:")
         label.grid(padx=5, pady=5)
 
@@ -69,11 +69,19 @@ class TaskView:
         delete_button = ttk.Button(master=self._frame, text="Delete selected task", command=self._handle_delete_button_click)
         delete_button.grid(column=3, sticky=(constants.E, constants.W))
 
+    def _show_task_list(self):
+
+        self._create_task_form()
+
+        self._all_tasks = self._get_all_tasks()
+
+        self._create_task_listbox()
+
         self._root.grid_columnconfigure(1, weight=1)
 
     def _handle_create_button_click(self):
-        title = self._task_title.get()
-        date = self._task_date.get()
+        title = self._task_title_entry.get()
+        date = self._task_date_entry.get()
 
         if title:
             task_service.create_task(title, date)
