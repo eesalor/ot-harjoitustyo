7b4dbf1a1e9769ef18c924e79603c72fe3df3094
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-16 12:13:32 +0300

    Edit UI: Show tasks in listbox and feature to delete selected task

--- src/ui/task_view.py
+++ src/ui/task_view.py
@@ -1,4 +1,6 @@
 from tkinter import ttk, constants
+from tkinter import *
+import tkinter as tk
 from services.task_service import task_service
 
 class TaskView:
@@ -9,6 +11,9 @@ class TaskView:
         self._task_title = None
         self._task_date = None
         self._all_tasks = None
+        self._listbox = None
+        self._listbox_dictionary = None
+        self._selected_task_id = None
 
         self._initialize()
     
@@ -42,20 +47,28 @@ class TaskView:
         self._task_date.grid(row=1, column=2, sticky=(constants.E, constants.W), padx=5, pady=5)
         
         button.grid(row=1, column=3, sticky=(constants.E, constants.W))
-        self._all_tasks = self._show_all_tasks()
+        self._all_tasks = self._get_all_tasks()
         
-        if len(self._all_tasks) != 0:
-            label = ttk.Label(master=self._frame, text="Task list:")
-            label.grid(padx=5, pady=5)
-            row = 2
+        label = ttk.Label(master=self._frame, text="Task list:")
+        label.grid(padx=5, pady=5)
 
-            for id, task in self._all_tasks.items():
-                task_label = ttk.Label(master=self._frame, text=task)
-                task_label.grid(row=row, column=1, sticky=(constants.W, constants.E), padx=5, pady=5)
-                delete_button = ttk.Button(master=self._frame, text="Delete", command=lambda id=id: self._handle_delete_button_click(id))
-                delete_button.grid(row=row, column=3, sticky=(constants.E, constants.W))
-                row += 1
-        
+        self._listbox = Listbox(master=self._frame)
+
+        self._listbox_dictionary = {}
+
+        n = 0
+        if len(self._all_tasks) != 0:
+            for row in self._all_tasks.items():
+                self._listbox_dictionary[n] = row
+                self._listbox.insert(tk.END, row[1])
+                n += 1
+
+        self._listbox.grid(column=1, sticky=(constants.E, constants.W))
+        self._listbox.bind('<<ListboxSelect>>', self._select_task)
+
+        delete_button = ttk.Button(master=self._frame, text="Delete selected task", command=self._handle_delete_button_click)
+        delete_button.grid(column=3, sticky=(constants.E, constants.W))
+
         self._root.grid_columnconfigure(1, weight=1)
 
     def _handle_create_button_click(self):
@@ -67,12 +80,17 @@ class TaskView:
 
         self._update_task_view()
 
-    def _handle_delete_button_click(self, id):
-        task_service.delete_task(id)
+    def _handle_delete_button_click(self):
+        if self._selected_task_id:
+            task_service.delete_task(self._selected_task_id)
 
-        self._update_task_view()
+            self._update_task_view()
 
-    def _show_all_tasks(self):
-        results = task_service.get_all_tasks()
+    def _get_all_tasks(self):
+        all_tasks = task_service.get_all_tasks()
         
-        return results
+        return all_tasks
+
+    def _select_task(self, event):
+        index = event.widget.curselection()[0]
+        self._selected_task_id = self._listbox_dictionary[index][0]
