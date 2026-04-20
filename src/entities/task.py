a9273fd27810c4149fa0ba694419cbc9ab5a1718
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-20 13:39:50 +0300

    Update repr method in class Task and remove str method

--- src/entities/task.py
+++ src/entities/task.py
@@ -4,11 +4,8 @@ class Task:
         self.date = date
         self.completed = completed
 
-    def __str__(self):
+    def __repr__(self):
         if self.completed is False:
             return f"{self.title} {self.date}"
 
         return f"{self.title} {self.date} (completed)"
-
-    def __repr__(self):
-        return f"{self.title} {self.date}"
