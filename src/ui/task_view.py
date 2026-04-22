9b36f9216c7da1348b3fedd93a6cd717e15c9437
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-22 12:58:41 +0300

    Refactor task view

--- src/ui/task_view.py
+++ src/ui/task_view.py
@@ -46,17 +46,69 @@ class TaskView:
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
 
-        self._show_new_task_form()
-
-        self._show_task_list()
-
-    def _show_new_task_form(self):
-
-        self._create_task_form()
+        self._initialize_task_creation_form()
 
         self._show_errors()
 
-    def _show_task_list(self):
+        self._show_task_listboxes()
+
+        set_completed_button = ttk.Button(
+            master=self._frame,
+            text="Set completed",
+            command=self._handle_set_completed_button_click
+            )
+
+        set_completed_button.grid(
+            column=1,
+            columnspan=2,
+            sticky=(constants.E, constants.W),
+            padx=5,
+            pady=5)
+
+        delete_button = ttk.Button(
+            master=self._frame,
+            text="Delete selected task",
+            command=self._handle_delete_uncompleted_button_click
+            )
+
+        delete_button.grid(
+            column=1,
+            columnspan=2,
+            sticky=(constants.E, constants.W),
+            padx=5,
+            pady=5
+            )
+
+        set_uncompleted_button = ttk.Button(
+            master=self._frame,
+            text="Set uncompleted",
+            command=self._handle_set_uncompleted_button_click
+            )
+
+        set_uncompleted_button.grid(
+            row=7,
+            column=3,
+            columnspan=2,
+            sticky=(constants.E, constants.W),
+            padx=5,
+            pady=5)
+
+        delete_button = ttk.Button(
+            master=self._frame,
+            text="Delete selected task",
+            command=self._handle_delete_completed_button_click
+            )
+
+        delete_button.grid(
+            row=8,
+            column=3,
+            columnspan=2,
+            sticky=(constants.E, constants.W),
+            padx=5,
+            pady=5
+            )
+
+    def _show_task_listboxes(self):
 
         self._uncompleted_tasks = self._get_uncompleted_tasks()
         self._completed_tasks = self._get_completed_tasks()
@@ -67,25 +119,72 @@ class TaskView:
         self._root.grid_columnconfigure(1, weight=1)
         self._root.grid_columnconfigure(2, weight=1)
 
-    def _create_task_form(self):
+    def _initialize_task_creation_form(self):
 
         create_label= ttk.Label(master=self._frame, text="Create new task")
+        create_label.grid(row=0, column=0, padx=5, pady=5)
+
+        self._initialize_task_field()
+
+        self._initialize_date_field()
+
+    def _initialize_task_field(self):
         task_title_label = ttk.Label(master=self._frame, text="Task*")
-        task_date_label = ttk.Label(master=self._frame, text="Date (dd.mm.yyyy)*")
 
         self._task_title_entry = ttk.Entry(master=self._frame)
+
+        task_title_label.grid(row=0, column=1, padx=5, pady=5)
+
+        self._task_title_entry.grid(
+            row=1,
+            column=1,
+            columnspan=2,
+            sticky=(constants.E, constants.W),
+            padx=5,
+            pady=5
+            )
+
+    def _initialize_date_field(self):
+        task_date_label = ttk.Label(master=self._frame, text="Date (dd.mm.yyyy)*")
+
         self._task_date_entry = ttk.Entry(master=self._frame)
 
-        create_button = ttk.Button(master=self._frame, text="Create", command=self._handle_create_button_click)
-
-        create_label.grid(row=0, column=0, padx=5, pady=5)
-        task_title_label.grid(row=0, column=1, padx=5, pady=5)
         task_date_label.grid(row=0, column=3, padx=5, pady=5)
 
-        self._task_title_entry.grid(row=1, column=1, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
-        self._task_date_entry.grid(row=1, column=3, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
+        self._task_date_entry.grid(
+            row=1,
+            column=3,
+            columnspan=2,
+            sticky=(constants.E, constants.W),
+            padx=5,
+            pady=5
+            )
+
+        create_button = ttk.Button(
+            master=self._frame,
+            text="Create",
+            command=self._handle_create_button_click
+            )
 
         create_button.grid(row=1, column=7, sticky=(constants.E, constants.W))
+
+    def _handle_create_button_click(self):
+        title = self._task_title_entry.get()
+        date = self._task_date_entry.get()
+
+        self._validate_task_title_entry(title)
+        self._validate_task_date_entry(date)
+
+        self._task_error_label_var.set(self._task_error_message)
+
+        self._date_error_label_var.set(self._date_error_message)
+
+        if self._task_error or self._date_error:
+            return
+
+        task_service.create_task(title, date)
+
+        self._update_task_view()
 
     def _show_errors(self):
         self._task_error_label_var = StringVar()
@@ -118,12 +217,6 @@ class TaskView:
         if len(self._uncompleted_tasks) != 0:
             self._listbox.bind('<<ListboxSelect>>', self._select_uncompleted_task)
 
-        set_completed_button = ttk.Button(master=self._frame, text="Set completed", command=self._handle_set_completed_button_click)
-        set_completed_button.grid(column=1, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
-
-        delete_button = ttk.Button(master=self._frame, text="Delete selected task", command=self._handle_delete_uncompleted_button_click)
-        delete_button.grid(column=1, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
-
     def _create_completed_task_listbox(self):
         label = ttk.Label(master=self._frame, text="Completed tasks:")
         label.grid(row=5, column=3, columnspan=2, padx=5, pady=5)
@@ -143,30 +236,6 @@ class TaskView:
 
         if len(self._completed_tasks) != 0:
             self._listbox_completed_tasks.bind('<<ListboxSelect>>', self._select_completed_task)
-
-        set_uncompleted_button = ttk.Button(master=self._frame, text="Set uncompleted", command=self._handle_set_uncompleted_button_click)
-        set_uncompleted_button.grid(row=7, column=3, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
-
-        delete_button = ttk.Button(master=self._frame, text="Delete selected task", command=self._handle_delete_completed_button_click)
-        delete_button.grid(row=8, column=3, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
-
-    def _handle_create_button_click(self):
-        title = self._task_title_entry.get()
-        date = self._task_date_entry.get()
-
-        self._validate_task_title_entry(title)
-        self._validate_task_date_entry(date)
-
-        self._task_error_label_var.set(self._task_error_message)
-
-        self._date_error_label_var.set(self._date_error_message)
-
-        if self._task_error or self._date_error:
-            return
-
-        task_service.create_task(title, date)
-
-        self._update_task_view()
 
     def _handle_delete_uncompleted_button_click(self):
         if self._selected_uncompleted_task_id:
