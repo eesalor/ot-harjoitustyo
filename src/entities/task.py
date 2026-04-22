dc3f9a859d6e95c0a48f0a9940d1a9d95bdd6a61
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-22 20:07:27 +0300

    New entity: class for category

--- src/entities/task.py
+++ src/entities/task.py
@@ -1,11 +1,12 @@
 class Task:
-    def __init__(self, title, date, completed=False):
+    def __init__(self, title, date, completed=False, category=None):
         self.title = title
         self.date = date
         self.completed = completed
+        self.category = category
 
     def __repr__(self):
         if self.completed is False:
-            return f"{self.title} {self.date}"
+            return f"{self.title} {self.date} {self.category}"
 
         return f"{self.title} {self.date} (completed)"
