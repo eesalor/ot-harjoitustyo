a6b3fb3a23f3b8f9d4744d4837ef2353aa8f13c2
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-24 18:16:56 +0300

    Fix unit tests in task repository

--- src/tests/task_repository_test.py
+++ src/tests/task_repository_test.py
@@ -8,38 +8,56 @@ class TestTaskRepository(unittest.TestCase):
         self.database = task_repository
         initialize_database()
 
-        self.task1 = Task("Write some unit tests", "2026-04-04")
-        self.task2 = Task("Refactor your code", "2026-05-05")
-        self.database.create_task(self.task1)
-        self.database.create_task(self.task2)
+        self.task1 = Task("Write some unit tests", "28.04.2026", False, 2)
+        self.task2 = Task("Refactor your code", "5.5.2026", False, None)
+        self.database.create_task(self.task1, 2)
+        self.database.create_task(self.task2, None)
 
-    def test_create_task(self):
-        task_title = "Write down working hours"
-        task_date = "2026-04-09"
-        self.task3 = Task(task_title, task_date)
-        self.database.create_task(self.task3)
-    
+    def test_create_task_without_category_id(self):
+        title = "Write down working hours"
+        date = "9.4.2026"
+        completed = False
+        category_id = None
+        self.task3 = Task(title, date, completed, category_id)
+        self.database.create_task(self.task3, category_id)
+
         self.assertEqual(self.task3.title, "Write down working hours")
-        self.assertEqual(self.task3.date, "2026-04-09")
+        self.assertEqual(self.task3.date, "9.4.2026")
+        self.assertEqual(self.task3.completed, False)
+        self.assertEqual(self.task3.category, None)
 
-    def test_get_all_tasks_returns_tasks(self):
-        tasks = self.database.get_all_tasks()
+    def test_create_task_with_category_id(self):
+        title = "Write down working hours"
+        date = "9.4.2026"
+        completed = False
+        category_id = 3
+        self.task3 = Task(title, date, completed, category_id)
+        self.database.create_task(self.task3, category_id)
 
-        self.assertEqual(tasks, {1: "Write some unit tests 2026-04-04", 2: "Refactor your code 2026-05-05"})
+        self.assertEqual(self.task3.title, "Write down working hours")
+        self.assertEqual(self.task3.date, "9.4.2026")
+        self.assertEqual(self.task3.completed, False)
+        self.assertEqual(self.task3.category, 3)
 
-    def test_get_completed_tasks(self):
+    def test_get_all_tasks_as_objects_returns_tasks(self):
+        tasks = self.database.get_all_tasks_as_objects()
+
+        self.assertEqual(repr(tasks[1]), "(Write some unit tests, 28.04.2026, 2, False)")
+        self.assertEqual(repr(tasks[2]), "(Refactor your code, 5.5.2026, None, False)")
+
+    def test_get_completed_tasks_without_categories(self):
         self.database.set_completed(1)
 
         completed_tasks = self.database.get_completed_tasks()
 
-        self.assertEqual(completed_tasks, {1: "Write some unit tests 2026-04-04"})
+        self.assertEqual(completed_tasks, {1: '(Write some unit tests, 28.04.2026, None, False)'})
 
     def test_get_uncompleted_tasks(self):
         self.database.set_completed(1)
 
         uncompleted_tasks = self.database.get_uncompleted_tasks()
 
-        self.assertEqual(uncompleted_tasks, {2: "Refactor your code 2026-05-05"})
+        self.assertEqual(uncompleted_tasks, {2: '(Refactor your code, 5.5.2026, None, False)'})
 
     def test_delete_task(self):
         self.database.delete_task(1)
@@ -65,8 +83,8 @@ class TestTaskRepository(unittest.TestCase):
         self.assertEqual(tasks[1].completed, False)
 
     def test_tasks_have_right_completed_status(self):
-        self.database.create_task(self.task1)
-        self.database.create_task(self.task2)
+        self.database.create_task(self.task1, 2)
+        self.database.create_task(self.task2, None)
 
         self.database.set_completed(1)
 
@@ -75,8 +93,6 @@ class TestTaskRepository(unittest.TestCase):
         self.assertEqual(tasks_as_objects[1].completed, True)
         self.assertEqual(tasks_as_objects[2].completed, False)
 
-        tasks = self.database.get_all_tasks()
-
-        self.assertEqual(tasks[1], f"{self.task1.title} {self.task1.date} (completed)")
-        self.assertEqual(tasks[2], f"{self.task2.title} {self.task2.date}")
+        self.assertEqual(repr(tasks_as_objects[1]), "(Write some unit tests, 28.04.2026, 2, True)")
+        self.assertEqual(repr(tasks_as_objects[2]), "(Refactor your code, 5.5.2026, None, False)")
 
