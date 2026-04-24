8fa7bf55463e9eb81e429d9724122e71740d9e5d
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-24 19:19:15 +0300

    Fix unit tests in task service / Add tests for setting tasks completed/uncompleted

--- src/tests/task_service_test.py
+++ src/tests/task_service_test.py
@@ -1,27 +1,51 @@
 import unittest
 from entities.task import Task
+from entities.category import Category
 from services.task_service import task_service
 from repositories.task_repository import task_repository
+from repositories.category_repository import category_repository
+from initialize_database import initialize_database
 
 class TestTaskService(unittest.TestCase):
     def setUp(self):
         self.service = task_service
         self.task_repository = task_repository
-        self.title = "Write some unit tests"
-        self.date = "2026-04-04"
+        self.category_repository = category_repository
 
-    def test_create_task(self):
-        result = self.service.create_task(self.title, self.date)
-        self.assertEqual(result, self.task_repository.create_task(Task(self.title, self.date)))
+        initialize_database()
+
+        self.title = "Write some unit tests"
+        self.date = "4.4.2030"
+        self.completed = False
+        self.category = "Studies"
+
+    def test_create_task_without_category(self):
+        category_none = None
+
+        result = self.service.create_task(self.title, self.date, category_none)
+
+        category_id = None
+
+        self.assertEqual(result, self.task_repository.create_task(Task(self.title, self.date), category_id))
 
     def test_get_all_tasks(self):
-        self.service.create_task(self.title, self.date)
         result = self.service.get_all_tasks()
 
-        self.assertEqual(result, self.task_repository.get_all_tasks())
+        self.assertEqual(result, self.task_repository.get_all_tasks_as_objects())
 
     def test_delete_task(self):
-        self.service.create_task(self.title, self.date)
         result = self.service.delete_task(1)
 
         self.assertEqual(result, self.task_repository.delete_task(1))
+
+    def test_set_completed(self):
+        result = self.service.set_completed(1)
+
+        self.assertEqual(result, self.task_repository.set_completed(1))
+
+    def test_set_uncompleted(self):
+        self.service.set_completed(1)
+
+        result = self.service.set_uncompleted(1)
+
+        self.assertEqual(result, self.task_repository.set_uncompleted(1))
