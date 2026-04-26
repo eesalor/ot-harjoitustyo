06ed6098bd670dffcabbc7e54b2c6964e5b0f60f
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-26 11:36:28 +0300

    Initial docstrings for task and category classes

--- src/entities/category.py
+++ src/entities/category.py
@@ -1,5 +1,16 @@
 class Category:
+    """Luokka, joka määrittelee aihenkategorian.
+
+    Attributes:
+        title: Merkkijonoarvo, joka kuvaa kategorian nimeä.
+    """
     def __init__(self, title):
+        """Luokan konstruktori, joka luo uuden kategorian.
+
+        Args:
+            title: Merkkijonoarvo, joka kuvaa kategorian nimeä.
+        """
+
         self.title = title
 
     def __repr__(self):
--- src/entities/task.py
+++ src/entities/task.py
@@ -1,5 +1,27 @@
 class Task:
+    """Luokka, joka määrittelee tehtävän.
+
+    Attributes:
+        title: Merkkijonoarvo, joka kuvaa tehtävän nimeä.
+        date: Merkkijonoarvo, joka kuvaa tehtävän määräaikaa.
+        completed: Boolen-arvo, joka kuvaa, onko tehtävä tehty vai ei.
+        category: Merkkijonoarvo, joka kuvaa tehtävän aihekategoriaa.
+    """
+
     def __init__(self, title, date, completed=False, category=None):
+        """Luokan konstruktori, joka luo uuden tehtävän.
+
+        Args:
+            title: Merkkijonoarvo, joka kuvaa tehtävän nimeä.
+            date: Merkkijonoarvo, joka kuvaa tehtävän määräaikaa.
+            completed:
+                Oletusarvoltaan False.
+                Boolen-arvo, joka kuvaa, onko tehtävä tehty vai ei.
+            category:
+                Oletusarvoltaan None.
+                Merkkijonoarvo, joka kuvaa tehtävän aihekategoriaa.
+        """
+
         self.title = title
         self.date = date
         self.completed = completed
