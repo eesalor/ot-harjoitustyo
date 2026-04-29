f0d01eaaad07ca8df4ceeb6bbc5c18bcb8e96352
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-29 17:55:20 +0300

    Add tests related to categories into task service

--- src/tests/task_service_test.py
+++ src/tests/task_service_test.py
@@ -10,6 +10,7 @@ class TestTaskService(unittest.TestCase):
     def setUp(self):
         self.service = task_service
         self.task_repository = task_repository
+        self.category_repository = category_repository
 
         initialize_database()
 
@@ -18,6 +19,8 @@ class TestTaskService(unittest.TestCase):
         self.completed = False
         self.category = "Studies"
 
+        self.category_repository.create_category(Category(self.category))
+
     def test_create_task_without_category(self):
         category_none = None
         result = self.service.create_task(self.title, self.date, category_none)
@@ -25,10 +28,21 @@ class TestTaskService(unittest.TestCase):
 
         self.assertEqual(result, self.task_repository.create_task(Task(self.title, self.date), category_id))
 
-    def test_get_tasks(self):
+    def test_create_task_with_category(self):
+        category = self.category
+        result = self.service.create_task(self.title, self.date, category)
+
+        self.assertEqual(result, self.task_repository.create_task(Task(self.title, self.date), 1))
+
+    def test_get_uncompleted_tasks(self):
         completion_status = self.completed
         result = self.service.get_tasks(completion_status)
-        self.assertEqual(result, self.task_repository.get_tasks())
+        self.assertEqual(result, self.task_repository.get_uncompleted_tasks())
+
+    def test_get_completed_tasks(self):
+        completion_status = True
+        result = self.service.get_tasks(completion_status)
+        self.assertEqual(result, self.task_repository.get_completed_tasks())
 
     def test_delete_task(self):
         result = self.service.delete_task(1)
@@ -45,3 +59,36 @@ class TestTaskService(unittest.TestCase):
         result = self.service.set_uncompleted(1)
 
         self.assertEqual(result, self.task_repository.set_uncompleted(1))
+
+    def test_get_categories_with_id(self):
+        result = self.service.get_categories_with_id()
+
+        self.assertEqual(result, self.category_repository.get_categories_with_id())
+
+    def test_get_categories(self):
+        result = self.service.get_categories()
+
+        self.assertEqual(result, self.category_repository.get_categories())
+
+    def test_create_category_with_existing_category(self):
+        category = "Studies"
+        self.service.create_category(category)
+
+        categories = self.service.get_categories()
+
+        self.assertEqual(len(categories), 1)
+
+    def test_create_category_with_non_existing_category(self):
+        category = "Work"
+        self.service.create_category(category)
+
+        categories = self.service.get_categories()
+
+        self.assertEqual(len(categories), 2)
+
+    def test_delete_category(self):
+        self.service.delete_category(self.category)
+
+        categories = self.service.get_categories()
+
+        self.assertEqual(len(categories), 0)
