3012c9527954aaffe86d6d158efed03e822525be
eesalor <197505674+eesalor@users.noreply.github.com>
2026-04-29 12:06:07 +0300

    Simplify str method in class Task

--- src/entities/task.py
+++ src/entities/task.py
@@ -28,16 +28,10 @@ class Task:
         self.category = category
 
     def __str__(self):
-        if self.completed is False:
-            if not self.category:
-                return f"{self.date:15} {self.title:30} (No category)"
+        if self.category:
+            return f"{self.date}, {self.title}, {self.category}"
 
-            return f"{self.date:15} {self.title:30} {self.category}"
-
-        if not self.category:
-            return f"{self.date:15} {self.title:30} (No category)"
-
-        return f"{self.date:15} {self.title:30} {self.category}"
+        return f"{self.date} {self.title} (No category)"
 
     def __repr__(self):
         return f"({self.title}, {self.date}, {self.category}, {self.completed})"
